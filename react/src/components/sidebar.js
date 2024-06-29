7b3fa2403f5b29eff45e2f5200d0f4e6673a8a6d
mislavmatic <148633611+mislavmatic@users.noreply.github.com>
2024-06-29 19:30:07 +0200

    Add files via upload

--- react/src/components/sidebar.js
+++ react/src/components/sidebar.js
@@ -1,1 +1,62 @@
+import React from "react";
+import "../App.css"
 
+import { useState } from 'react';
+import FileUpload from '../FileUpload';
+
+function Sidebar({ setSelectedGroup }) {
+  const [data, setData] = useState(null);
+  const [expandedAsistenti, setExpandedAsistenti] = useState({});
+  const [expandedGrupe, setExpandedGrupe] = useState({});
+
+  const handleGetData = () => {
+      fetch('http://localhost:5000/get_data')
+          .then(response => response.json())
+          .then(data => setData(data))
+          .catch(error => console.error('Error:', error));
+  };
+
+  const toggleExpandedAsistenti = (id) => {
+      setExpandedAsistenti(prevExpanded => {
+          const newExpanded = {...prevExpanded};
+          newExpanded[id] = !newExpanded[id];
+          return newExpanded;
+      });
+  };
+
+  const toggleExpandedGrupe = (id) => {
+      setExpandedGrupe(prevExpanded => {
+          const newExpanded = {...prevExpanded};
+          newExpanded[id] = !newExpanded[id];
+          return newExpanded;
+      });
+  };
+
+  return (
+      <div className="Sidebar">
+          <FileUpload />
+          <button onClick={handleGetData}>Get Data</button>
+          {data && data.map((assistant, assistantIndex) => (
+              <div key={assistant.name_asist} className="assistant">
+                  <button onClick={() => toggleExpandedAsistenti(assistantIndex)}>
+                      {assistant.name_asist}
+                  </button>
+                  {expandedAsistenti[assistantIndex] && assistant.groups.map((group, groupIndex) => (
+                      <div key={group.name_g} className="group">
+                          <button onClick={() => {
+                              toggleExpandedGrupe(groupIndex);
+                              setSelectedGroup(group);
+                          }}>
+                              {group.name_g}
+                          </button>
+                          {expandedGrupe[groupIndex]}
+                      </div>
+                  ))}
+              </div>
+          ))}
+      </div>
+  );
+}
+
+
+export default Sidebar
