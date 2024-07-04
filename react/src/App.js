0b59e7a2ef015117d4ae4b1866e4094b4bdd83c6
mislavmatic <148633611+mislavmatic@users.noreply.github.com>
2024-07-04 17:44:47 +0200

    Add files via upload

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-
 import Sidebar from './components/sidebar';
 import React, { useState } from 'react';
 
@@ -10,21 +9,20 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        Naslov web aplikacije
-      </header> */}
       <Sidebar setSelectedGroup={setSelectedGroup} />
       {selectedGroup && (
         <div className="group-info">
           <h2>{selectedGroup.name_g}</h2>
           <p>GitHub: <a href={selectedGroup.github}>{selectedGroup.github}</a></p>
           <p>Zadatak: {selectedGroup.name_zad}</p>
-          {/* <p>Demos: {selectedGroup.name_demos && selectedGroup.name_demos.join(', ')}</p> */}
           <p>Demos: {selectedGroup.name_demos}</p>          
-          {/* <p>Students: {selectedGroup.students && selectedGroup.students.join(', ')}</p> */}
           <div>Studenti: <div style={{ marginLeft: '2vh' }}>
                           {selectedGroup.students.map(student => (
-                            <p key={student.s_id} className="student">{student.name} {student.is_leader ? "(voditelj)" : ""}</p>
+                            <div key={student.s_email} className="student">
+                              <p style={{ width: '10vw', display: 'inline-block', marginRight: "1vw"}}>{student.name}</p>
+                              <p style={{ width: '10vw', display: 'inline-block', marginRight: "1vw"}}>{student.stud_email}</p>
+                              <p style={{ width: '10vw', display: 'inline-block', marginRight: "1vw"}}>{student.is_leader ? "(voditelj)" : ""}</p>
+                            </div>
                           ))} 
                         </div>
           </div>
